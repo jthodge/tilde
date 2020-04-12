4bdc9c10314b686d2b28c151a1017ee8cd19809c
Taylor Hodge <j.taylor.hodge@gmail.com>
2020-04-12 19:09:58 -0400

    feat: add project directory

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -34,6 +34,7 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
+(setq projectile-project-search-path '("~/workos/"))
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
