3132b50ca59b045376418f6295b5d9bdee81b3b5
Taylor Hodge <j.taylor.hodge@gmail.com>
2022-10-29 00:42:21 -0400

    emacs: remove cruft

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(wombat))
  '(custom-safe-themes
    '("3a9f65e0004068ecf4cf31f4e68ba49af56993c20258f3a49e06638c825fbfb6" "ddffe74bc4bf2c332c2c3f67f1b8141ee1de8fd6b7be103ade50abb97fe70f0c" "28a34dd458a554d34de989e251dc965e3dc72bace7d096cdc29249d60f395a82" "b1e95054aec25f5c9ced81c0f8c303482c222a75b931720b2c174d84a912bb70" "a08f552b2cc19d45c2ebf8967decc2d976f04d14b46f473e88bb972d2a31eafd" default))
  '(package-selected-packages '(ujelly-theme rainbow-delimiters paredit markdown-mode)))
