c1a6ea6eefede6c1146712b7ed55e2c20e0e2c60
Taylor Hodge <j.taylor.hodge@gmail.com>
2025-03-18 18:59:32 -0700

    feat: update emacs python env

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -45,6 +45,128 @@
 (setq show-paren-delay 0)
 (show-paren-mode 1)
 
+;; Package Support
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defconst my-packages
+  '(company                ; Code and text completion framework
+    consult                ; Incremental narrowing
+    consult-lsp            ; Improve working between `consult` and `lsp-mode`
+    dap-mode               ; Debug Adapter Protocol Support
+    flycheck               ; Linting and syntax checker
+    lsp-mode               ; Language Server Protocol support
+    lsp-pyright            ; Language Server Protocol client using pyright Python Language Server
+    lsp-ui                 ; UI improvements for `lsp-mode`
+    which-key              ; Display currently available keybindings
+    yasnippet              ; Snippet and template management
+    ))
+
+(mapc #'(lambda (package)
+          (unless (package-installed-p package)
+            (package-install package)))
+      my-packages)
+
+;; Set standard action keymaps
+;; N.B. These prevent conflicts with `lsp-mode` keymaps
+(global-set-key (kbd "C-c o l") #'org-store-link)
+(global-set-key (kbd "C-c o b") #'org-switchb)
+
+;; Text Completion
+(require 'company)
+(setopt company-idle-delay 0.1)
+(setopt company-tooltip-align-annotations t)
+(defmacro company-backend-for-hook (hook backends)
+  `(add-hook ,hook (lambda ()
+                     (set (make-local-variable 'company-backends)
+                          ,backends))))
+
+;; Snippets and template expansion
+(with-eval-after-load 'yasnippet
+  (yas-reload-all))
+
+;; LSP
+;; Set `lsp-mode` keymap prefix
+(setopt lsp-keymap-prefix "C-c l")
+;; Shut down LSP server when all buffers associated with the server are closed
+(setopt lsp-keep-workspace-alive nil)
+;; Configure breadcrumbs
+(setopt lsp-headerline-breadcrumb-segments
+        '(path-up-to-project
+          file
+          symbols))
+;; Set delay for `lsp-ui` doc overlay
+(setopt lsp-ui-doc-delay 0.5)
+;; Set `lsp-mode` to use `flycheck` (instead of `flymake`) as diagnostics provider
+(setopt lsp-diagnostics-provider :flycheck)
+
+(with-eval-after-load 'lsp-ui
+  ;; Map `xref-find-definitions` (Default: `M-.`)
+  (define-key lsp-ui-mode-map
+              [remap xref-find-definitions]
+              #'lsp-ui-peek-find-definitions)
+
+  ;; Map `xref-find-references` (Default: `M-?`)
+  (define-key lsp-ui-mode-map
+              [remap xref-find-references]
+              #'lsp-ui-peek-find-references))
+
+;; Configure `lsp-mode` enhancements
+(with-eval-after-load 'lsp-mode
+  ;; Remap `lsp-treemacs-errors-list` to `C-c l g e`
+  (define-key lsp-mode-map
+              [remap lsp-treemacs-errors-list]
+              #'consult-lsp-diagnostics)
+  ;; Remap `xref-find-apropos` to `C-c l g a`
+  (define-key lsp-mode-map [remap xref-find-apropos] #'consult-lsp-symbols)
+  ;; Enable `which-key-mode` `lsp-mode` integration
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration))
+
+;; Configure debugging
+(setopt dap-auto-configure-mode t)
+
+(defmacro company-backend-for-hook (hook backends)
+  `(add-hook ,hook (lambda ()
+                     (set (make-local-variable 'company-backends)
+                          ,backends))))
+
+;; Python
+;; Load `python` mode when openin Python files
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+
+;; Set indentation
+(setq python-indent-offset 4)
+
+(defun set-up-python-env ()
+  "Set up Python development environment in current buffer."
+  (company-backend-for-hook 'lsp-completion-mode-hook
+                        '((company-capf :with company-yasnippet)
+                          company-dabbrev-code))
+
+  ;; Enable LSP support in Python buffers
+  (require 'lsp-pyright)
+
+  ;; Start LSP server when switching to buffer
+  (lsp-deferred)
+
+  ;; Enable yasnippet minor mode only in python-mode
+  (yas-minor-mode 1)
+
+  ;; Find and set appropriate Python executable
+  (setq-local python-shell-interpreter (executable-find "python"))
+
+  ;; Enable DAP support in Python buffers
+  (require 'dap-python)
+  (setq-local dap-python-debugger 'debugpy)
+  (dap-mode 1))
+
+;; Configure hooks after `python-mode` is loaded
+(add-hook 'python-mode-hook #'set-up-python-env)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -54,7 +176,8 @@
    '("f747c4004e38bcdc131649a90325c00d246bb7dc73bc6ab6e0e7ab5489da8459"
      "a4340c197a450c77c729cad236b5f3ca88aaf974e91a7af2d2e7ae7bb5f96720"
      "6b20d669fcbcd79c6d0f3db36a71af1b88763246d3550a0c361866adecb38a9e"
-     default)))
+     default))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
